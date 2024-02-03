039907ed4a6f4389796d091a2014f80c1b843af8
Bogdan Ionescu <bogdan.ionescu.work@gmail.com>
2024-02-03 15:46:50 +0000

    ncam opens

--- pref_edit.py
+++ pref_edit.py
@@ -13,7 +13,7 @@ import re
 import gi
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk, Pango, GdkPixbuf  # noqa: E402
+from gi.repository import Gtk, Gdk, GdkPixbuf  # noqa: E402
 
 translate_test = True
 
