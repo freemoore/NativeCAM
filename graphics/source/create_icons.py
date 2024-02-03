83ddec4d1aa1ce32fd3f244613a64ea13e5467e5
Bogdan Ionescu <bogdan.ionescu.work@gmail.com>
2024-02-03 15:49:15 +0000

    create_icons: migrate to python3

--- graphics/source/create_icons.py
+++ graphics/source/create_icons.py
@@ -21,43 +21,45 @@
 ##########################################
 
 import sys
-
-import pygtk
-pygtk.require('2.0')
-
-from lxml import etree
 import os
 import getopt
+from lxml import etree
 
-optlist, args = getopt.getopt(sys.argv[1:], 'w:r')
+optlist, args = getopt.getopt(sys.argv[1:], "w:r")
 
 optlist = dict(optlist)
 renew_all = "-r" in optlist
 
 xml = etree.parse("icons.svg")
-for x in xml.findall(".//{http://www.w3.org/2000/svg}title") :
-	try :
-		id_ = x.getparent().get("id")
-		if not os.path.isfile("../%s.png" % (x.text)) or renew_all :
-			w = float(os.popen("inkscape icons.svg --query-id=%s --query-width " % id_).read())
-			h = float(os.popen("inkscape icons.svg --query-id=%s --query-height" % id_).read())
+for x in xml.findall(".//{http://www.w3.org/2000/svg}title"):
+    try:
+        id_ = x.getparent().get("id")
+        if not os.path.isfile("../%s.png" % (x.text)) or renew_all:
+            w = float(
+                os.popen("inkscape icons.svg --query-id=%s --query-width " % id_).read()
+            )
+            h = float(
+                os.popen("inkscape icons.svg --query-id=%s --query-height" % id_).read()
+            )
 
-			if w > h :
-				w, h = 80, 80 * h / w
-			else :
-				h, w = 80, 80 * w / h
-			try :
-				s = "inkscape icons.svg --export-png=../%s.png --export-id-only --export-id=%s --export-area-snap --export-width=%spx --export-height=%spx " % (x.text, id_, w, h)
-				print os.popen(s).read()
-				print "Created %s" % x.text
-			except Exception, e :
-				print e
-		else : print "Skipping %s" % x.text
-	except Exception, e :
-		print
-		print "Error with the file %s.png!" % (x.text)
-		print e
-		print
-	print
-
-
+            if w > h:
+                w, h = 80, 80 * h / w
+            else:
+                h, w = 80, 80 * w / h
+            try:
+                s = (
+                    "inkscape icons.svg --export-png=../%s.png --export-id-only --export-id=%s --export-area-snap --export-width=%spx --export-height=%spx "
+                    % (x.text, id_, w, h)
+                )
+                print(os.popen(s).read())
+                print("Created %s" % x.text)
+            except Exception as e:
+                print(e)
+        else:
+            print("Skipping %s" % x.text)
+    except Exception as e:
+        print()
+        print("Error with the file %s.png!" % (x.text))
+        print(e)
+        print()
+    print()
